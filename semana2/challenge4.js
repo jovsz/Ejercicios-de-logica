3db1cab6a176347d16fe12da4c1da351e7687c8d
jovsz <jovannyfuentesm@gmail.com>
2021-09-02 23:49:37 -0700

    semana2/challenge4 incomplete

--- semana2/challenge4.js
+++ semana2/challenge4.js
@@ -1,15 +1,15 @@
 function landMass(name, area) {
-    //Implementación
-    const info = [
-        {
-            percent: '', 
-            message: '',
-        }
-    ];
-    const percent = '';
+    //Implementación    
     const worldMasa = 148940000;
-
+    let info = [];
+    if(area){
+        let percent = (area*100)/worldMasa;
+        const floatNum = parseFloat(percent);
+        info = [{percent: `${floatNum}`, message: `${name} representa el ${floatNum}% de la masa de la tierra`}]
+        return info[0];
+    }
     return 0;
 }
+
 
 module.exports = landMass;
